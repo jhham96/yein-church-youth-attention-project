78745f97c24a6e15106235e4e18e0c458d18cce6
jhham96 <logbug1004@gmail.com>
2026-03-17 12:45:29 +0900

    위치 쉽게 변경하게끔 수정

--- config.js
+++ config.js
@@ -5,7 +5,13 @@ const CONFIG = {
     "https://script.google.com/macros/s/AKfycbwtT5z_4MHA6KMXhAPPmddUw-OAsOsd9rriMZ-iOW8_MUY0O5dR4baUZoq6d0823Gu6Sw/exec",
 
   // 2. 모임 장소 설정 (위도, 경도, 허용 반경)
-  TARGET_LAT: 37.568208, // 위도(예인교회)
-  TARGET_LON: 127.036794, // 경도(예인교회)
+  TARGET_LAT: lat_lon_group["삼성본관"].lat, // 위도
+  TARGET_LON: lat_lon_group["삼성본관"].lon, // 경도
   MAX_DISTANCE: 50, // 허용 거리 (미터)
+};
+
+const lat_lon_group = {
+  삼성본관: { lat: 37.562315, lon: 126.975602 },
+  예인교회: { lat: 37.568208, lon: 127.036794 },
+  // 다른 장소들도 추가 가능
 };
