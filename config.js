06214a921056e7e061c5bbdfe47b1dca7e2c020b
jhham96 <logbug1004@gmail.com>
2026-03-17 12:42:36 +0900

    config 파일로 구분 + 브라우저 안타고 기기 고유값으로 중복제어 되도록 보완

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+const CONFIG = {
+  // 1. 앱스 스크립트 웹 앱 URL
+  SCRIPT_URL:
+    "https://script.google.com/macros/s/AKfycbwtT5z_4MHA6KMXhAPPmddUw-OAsOsd9rriMZ-iOW8_MUY0O5dR4baUZoq6d0823Gu6Sw/exec",
+
+  // 2. 모임 장소 설정 (위도, 경도, 허용 반경)
+  TARGET_LAT: 37.568208, // 위도(예인교회)
+  TARGET_LON: 127.036794, // 경도(예인교회)
+  MAX_DISTANCE: 50, // 허용 거리 (미터)
+};
