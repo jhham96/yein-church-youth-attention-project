86a73774043098d284df3b9b5e6d96a603bc8f56
jhham96 <logbug1004@gmail.com>
2026-03-21 18:35:09 +0900

    중심지 위치와 반경 노출 및 이름 오입력을 방지한 컨펌창 추가 + 디자인 업데이트

--- config.js
+++ config.js
@@ -6,13 +6,16 @@ const lat_lon_group = {
   // 다른 장소들도 추가 가능
 };
 
+// 💡 여기서 모임 장소 이름만 바꿔주면 됩니다!
+const TARGET_PLACE = "예인교회";
+
 const CONFIG = {
   // 1. 앱스 스크립트 웹 앱 URL
   SCRIPT_URL:
     "https://script.google.com/macros/s/AKfycbwtT5z_4MHA6KMXhAPPmddUw-OAsOsd9rriMZ-iOW8_MUY0O5dR4baUZoq6d0823Gu6Sw/exec",
 
   // 2. 모임 장소 설정 (위도, 경도, 허용 반경)
-  TARGET_LAT: lat_lon_group["예인교회"].lat, // 위도
-  TARGET_LON: lat_lon_group["예인교회"].lon, // 경도
+  TARGET_LAT: lat_lon_group[TARGET_PLACE].lat, // 위도
+  TARGET_LON: lat_lon_group[TARGET_PLACE].lon, // 경도
   MAX_DISTANCE: 50, // 허용 거리 (미터)
 };
