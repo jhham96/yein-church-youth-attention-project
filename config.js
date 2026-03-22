d5e92dc2a955a5f79c03417adf8aee44bb4753b5
jhham96 <logbug1004@gmail.com>
2026-03-22 23:58:05 +0900

    시간 제어 로직 추가 배포

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const TARGET_PLACE = "예인교회";
 const CONFIG = {
   // 1. 앱스 스크립트 웹 앱 URL
   SCRIPT_URL:
-    "https://script.google.com/macros/s/AKfycbwtT5z_4MHA6KMXhAPPmddUw-OAsOsd9rriMZ-iOW8_MUY0O5dR4baUZoq6d0823Gu6Sw/exec",
+    "https://script.google.com/macros/s/AKfycbw-bSTInlh0M0tPjJioYuTbN4jWV0_erWauYXY2V9eOyP52K_9Q_C7Ha2XtQr4QTQealQ/exec",
 
   // 2. 모임 장소 설정 (위도, 경도, 허용 반경)
   TARGET_LAT: lat_lon_group[TARGET_PLACE].lat, // 위도
