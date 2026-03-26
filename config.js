0ff468d786b637819bf4225418e400eb67e246f3
jhham96 <logbug1004@gmail.com>
2026-03-26 23:33:37 +0900

    IP기록 추가 및 출석 가능 9시 이후에도 가능하지만 서버에서 9시 이후 사람 거르도록 수정 / 테스트 위해 집으로 잠시 위치 수정

--- config.js
+++ config.js
@@ -3,16 +3,17 @@
 const lat_lon_group = {
   삼성본관: { lat: 37.562315, lon: 126.975602 },
   예인교회: { lat: 37.568208, lon: 127.036794 },
+  우리집: { lat: 37.563481, lon: 127.055696 },
   // 다른 장소들도 추가 가능
 };
 
 // 💡 여기서 모임 장소 이름만 바꿔주면 됩니다!
-const TARGET_PLACE = "예인교회";
+const TARGET_PLACE = "우리집";
 
 const CONFIG = {
   // 1. 앱스 스크립트 웹 앱 URL
   SCRIPT_URL:
-    "https://script.google.com/macros/s/AKfycbw-bSTInlh0M0tPjJioYuTbN4jWV0_erWauYXY2V9eOyP52K_9Q_C7Ha2XtQr4QTQealQ/exec",
+    "https://script.google.com/macros/s/AKfycbx2UKtGGILYcYvKfwaKJThcQsCKd0q2Jd5ISXFJ-Acv-hhvSSjr-pE5QbfEyu96CvmcXQ/exec",
 
   // 2. 모임 장소 설정 (위도, 경도, 허용 반경)
   TARGET_LAT: lat_lon_group[TARGET_PLACE].lat, // 위도
