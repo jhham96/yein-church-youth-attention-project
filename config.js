c7e9fdde0b97193b8b3a032cbd2024bfe1415932
jhham96 <logbug1004@gmail.com>
2026-03-29 10:17:21 +0900

    11시로 늘림(서버변경)

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const TARGET_PLACE = "예인교회";
 const CONFIG = {
   // 1. 앱스 스크립트 웹 앱 URL
   SCRIPT_URL:
-    "https://script.google.com/macros/s/AKfycbzhlCgFnb7xljoxfRRl1M_EfOg3wuI_-E81NWUXiGqNjKhHzQWRmQoLnbE54RKzeK11QA/exec",
+    "https://script.google.com/macros/s/AKfycbzwEARzXbpH9EUXaE4san-IEzf2op3j910x-E09SKHvDvPU6b2yrTY51UdNt4iYl0ijoA/exec",
 
   // 2. 모임 장소 설정 (위도, 경도, 허용 반경)
   TARGET_LAT: lat_lon_group[TARGET_PLACE].lat, // 위도
