41ff0962bd94ec71800ae9d01246a01db0f9c9b4
jhham96 <logbug1004@gmail.com>
2026-04-06 00:38:54 +0900

    서버 수정 및 테스트 하기 위해 위치 잠시 변경

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ const lat_lon_group = {
 };
 
 // 💡 여기서 모임 장소 이름만 바꿔주면 됩니다!
-const TARGET_PLACE = "예인교회";
+const TARGET_PLACE = "우리집";
 
 const CONFIG = {
   // 1. 앱스 스크립트 웹 앱 URL
   SCRIPT_URL:
-    "https://script.google.com/macros/s/AKfycbzwEARzXbpH9EUXaE4san-IEzf2op3j910x-E09SKHvDvPU6b2yrTY51UdNt4iYl0ijoA/exec",
+    "https://script.google.com/macros/s/AKfycbytlNQTCfms3b5KGJ9unwawMaV5hWM9o5OFiIPxL16zebQk_yaH5izMahQezqMDXUX_6g/exec",
 
   // 2. 모임 장소 설정 (위도, 경도, 허용 반경)
   TARGET_LAT: lat_lon_group[TARGET_PLACE].lat, // 위도
@@ -23,6 +23,13 @@ const CONFIG = {
 
 // config.js 하단에 추가
 const PATCH_NOTES = [
+  {
+    version: "v1.5",
+    date: "2026-04-05",
+    changes: [
+      "GPS 실패시 거리정보 같이 전달하여 우선적으로 출석할수 있도록 수정.",
+    ],
+  },
   {
     version: "v1.4",
     date: "2026-03-26",
